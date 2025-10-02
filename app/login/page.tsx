482dc98242da872398ccf36cb5f7552b06006ba0
Angelus23 <izerefaifelix@gmail.com>
2025-10-02 02:10:20 +0200

    Improve login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -272,7 +272,7 @@ export default function LoginPage() {
                     <div className="w-full border-t border-gray-200"></div>
                   </div>
                   <div className="relative flex justify-center text-sm">
-                    <span className="px-4 bg-white/90 text-gray-500">Insight Art Space?</span>
+                    <span className="px-4 bg-white/90 text-gray-500">Insight Art Space</span>
                   </div>
                 </div>
                 {/* <Link 
